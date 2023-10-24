a67839cf19e195cebc28bdf2777b26957e78a1fe
Hungnguyen1105 <hbday2k3@gmail.com>
2023-10-24 15:56:11 +0700

    update

--- routes/home.js
+++ routes/home.js
@@ -6,15 +6,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  // SQL : SELECT * FROM student
   var bearbricks = await BearbrickModel.find();
   var gokus = await GokuModel.find();
-  console.log(bearbricks);
-  //res.send(students);
-  // render ra file view : views/student/index.hbs và gửi kèm data thông qua biến 'students'
   res.render('home/index', { 
      bearbricks: bearbricks,
      gokus: gokus,
    });
 })
+
+router.get('')
 module.exports = router;
