b053c66683c818a6a4a4ac3acd2e2f53b5be380d
Hungnguyen1105 <hbday2k3@gmail.com>
2023-10-27 20:18:03 +0700

    update

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ mongoose.set('strictQuery', true);
 mongoose.connect(uri)
   .then(() => console.log('connect to db ok'))
   .catch((err) => console.log('connect to db error'));
+var hbs = require('hbs');
+hbs.registerHelper('dateFormat', require('handlebars-dateformat'));
+
 
 var app = express();
 
@@ -36,18 +39,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/product' ,productRouter)
+app.use('/product', productRouter)
 app.use('/home', homeRouter);
 app.use('/login', loginRouter)
 app.use('/bearbrick', bearbrickRouter)
 app.use('/goku', gokuRouter)
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -56,6 +59,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(process.env.PORT||3001);
+app.listen(process.env.PORT || 3001);
 
 module.exports = app;
